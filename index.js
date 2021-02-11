079664fa0851b99480639270f2b18b2410db58df
Zakaria Boussoffara <zakaria.boussoffara@polymtl.ca>
2021-02-11 13:59:27 -0500

    init commit

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-const http = require('http');
+"use strict";
 
-const server = http.createServer((request, response) => {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
+const serverPort = 8081,
+    http = require("http"),
+    express = require("express"),
+    app = express(),
+    server = http.createServer(app),
+    WebSocket = require("ws"),
+    websocketServer = new WebSocket.Server({ server });
+
+//when a websocket connection is established
+websocketServer.on('connection', (webSocketClient) => {
+    //send feedback to the incoming connection
+    webSocketClient.send('{ "connection" : "ok"}');
+
+    //when a message is received
+    webSocketClient.on('message', (message) => {
+
+        //for each websocket client
+        websocketServer
+            .clients
+            .forEach(client => {
+                //send the client the current message
+                client.send(`{ "message" : ${message} }`);
+            });
+    });
 });
 
-const port = process.env.PORT || 1337;
-server.listen(port);
-
-console.log("Server running at http://localhost:%d", port);
+//start the web server
+server.listen(serverPort, () => {
+    console.log(`Websocket server started on port ` + serverPort);
+});
